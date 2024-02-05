b931c7e72ce9a40e646d05df521708d1669bd9c3
MehrdadKhorasani <mehrdadaria9@gmail.com>
2024-02-05 16:55:11 +0330

    start to add the logic

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ const current1 = document.getElementById("current--1");
 
 const dice0 = document.getElementById("dice--0");
 const dice1 = document.getElementById("dice--1");
+const dice0I = document.getElementById("dice--0-i");
+const dice1I = document.getElementById("dice--1-i");
 
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
@@ -16,7 +18,7 @@ const btnHold = document.querySelector(".btn--hold");
 
 let scores, currentScore, activePlayer, playing;
 
-const init = function () {
+function init() {
   scores = [0, 0];
   currentScore = 0;
   activePlayer = 0;
@@ -29,6 +31,48 @@ const init = function () {
 
   dice0.classList.add("hidden");
   dice1.classList.add("hidden");
-};
+}
 
 init();
+
+function switchPlayer() {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+}
+
+btnRoll.addEventListener("click", function () {
+  if (playing) {
+    const firstDice = Math.trunc(Math.random() * 6) + 1;
+    const secondDice = Math.trunc(Math.random() * 6) + 1;
+    dice0.classList.remove("hidden");
+    dice0.src = `img/dice-${firstDice}.png`;
+    dice1.classList.remove("hidden");
+    dice1.src = `img/dice-${secondDice}.png`;
+
+    if (firstDice + secondDice != 7) {
+      currentScore += firstDice + secondDice;
+      document.getElementById("current--0").textContent = currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener("click", function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    if (scores[activePlayer] >= 100) {
+      // finish the game
+      playing = false;
+      diceEl.classList.add("hidden");
+    } else {
+      // switch to the next player
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener("click", init);

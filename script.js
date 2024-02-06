43929cd333aef600ff99b0bf79a7567c8e952502
MehrdadKhorasani <mehrdadaria9@gmail.com>
2024-02-06 22:40:25 +0330

    add a little logic and style

--- script.js
+++ script.js
@@ -6,15 +6,17 @@ const score0 = document.getElementById("score--0");
 const score1 = document.getElementById("score--1");
 const current0 = document.getElementById("current--0");
 const current1 = document.getElementById("current--1");
+const gameStatus = document.querySelector(".status-bar");
+
+const playerName = document.getElementById("name--0");
+const cpuName = document.getElementById("name--1");
 
 const dice0 = document.getElementById("dice--0");
 const dice1 = document.getElementById("dice--1");
-const dice0I = document.getElementById("dice--0-i");
-const dice1I = document.getElementById("dice--1-i");
 
-const btnNew = document.querySelector(".btn--new");
-const btnRoll = document.querySelector(".btn--roll");
-const btnHold = document.querySelector(".btn--hold");
+const keyNew = document.querySelector(".key--new");
+const keyRoll = document.querySelector(".key--roll");
+const keyHold = document.querySelector(".key--hold");
 
 let scores, currentScore, activePlayer, playing;
 
@@ -29,19 +31,50 @@ function init() {
   current0.textContent = 0;
   current1.textContent = 0;
 
+  gameStatus.textContent = "Roll the Dice ...";
+
   dice0.classList.add("hidden");
   dice1.classList.add("hidden");
 }
 
 init();
 
+document.addEventListener("keydown", function (e) {
+  if (e.key !== "r") return;
+  roll();
+});
+document.addEventListener("keydown", function (e) {
+  if (e.key !== "h") return;
+  hold();
+});
+document.addEventListener("keydown", function (e) {
+  if (e.key !== "n") return;
+  init();
+});
+
 function switchPlayer() {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
+
+  if (activePlayer === 0) {
+    playerName.classList.add("active");
+    cpuName.classList.remove("active");
+  } else {
+    cpuName.classList.add("active");
+    playerName.classList.remove("active");
+  }
 }
 
-btnRoll.addEventListener("click", function () {
+function rullChecker(firstDice, secondDice, score) {
+  if (firstDice + secondDice === 7) return "loseCurrent";
+  if (firstDice === 1 && secondDice === 1) return "loseTotal";
+  if (firstDice === secondDice) return "mustRoll";
+  if (firstDice + secondDice + score === 100) "loseTotal";
+  return "OK";
+}
+
+function roll() {
   if (playing) {
     const firstDice = Math.trunc(Math.random() * 6) + 1;
     const secondDice = Math.trunc(Math.random() * 6) + 1;
@@ -50,29 +83,44 @@ btnRoll.addEventListener("click", function () {
     dice1.classList.remove("hidden");
     dice1.src = `img/dice-${secondDice}.png`;
 
-    if (firstDice + secondDice != 7) {
-      currentScore += firstDice + secondDice;
-      document.getElementById("current--0").textContent = currentScore;
-    } else {
-      switchPlayer();
+    const conclusion = rullChecker(firstDice, secondDice, scores[activePlayer]);
+    keyHold.classList.remove("hidden");
+    switch (conclusion) {
+      case "loseCurrent":
+        current0.textContent = 0;
+        switchPlayer();
+        break;
+      case "loseTotal":
+        scores[activePlayer] = 0;
+        score0.textContent = 0;
+        current0.textContent = 0;
+        switchPlayer();
+        break;
+      case "mustRoll":
+        keyHold.classList.add("hidden");
+        gameStatus.textContent = "Show Must Go on";
+        currentScore += firstDice + secondDice;
+        document.getElementById("current--0").textContent = currentScore;
+        break;
+      case "OK":
+        currentScore += firstDice + secondDice;
+        document.getElementById(`current--${activePlayer}`).textContent =
+          currentScore;
     }
   }
-});
+}
 
-btnHold.addEventListener("click", function () {
+function hold() {
   if (playing) {
     scores[activePlayer] += currentScore;
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     if (scores[activePlayer] >= 100) {
-      // finish the game
       playing = false;
-      diceEl.classList.add("hidden");
+      dice0.classList.add("hidden");
+      dice1.classList.add("hidden");
     } else {
-      // switch to the next player
       switchPlayer();
     }
   }
-});
-
-btnNew.addEventListener("click", init);
+}

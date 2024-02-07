a841e4ee66ac16f1476c38cf33fab89a58d219ff
MehrdadKhorasani <mehrdadaria9@gmail.com>
2024-02-07 22:41:16 +0330

    add the menu and some of its functionality

---
+++ JS/menu.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const menuView = document.querySelector(".main-menu");
+const gameView = document.querySelector(".main-game");
+
+const menuCpu = document.getElementById("menu-item-cpu");
+const menuTwo = document.getElementById("menu-item-two");
+const menuInstruct = document.getElementById("menu-item-ins");
+
+window.addEventListener("load", function () {
+  gameView.classList.add("hiddenView");
+  menuView.classList.remove("hiddenView");
+});
+
+document.addEventListener("keydown", function (e) {
+  if (menuCpu.classList.contains("active-item") && e.key === "ArrowDown") {
+    menuCpu.classList.remove("active-item");
+    menuTwo.classList.add("active-item");
+  } else if (
+    menuTwo.classList.contains("active-item") &&
+    e.key === "ArrowDown"
+  ) {
+    menuTwo.classList.remove("active-item");
+    menuInstruct.classList.add("active-item");
+  } else if (menuTwo.classList.contains("active-item") && e.key === "ArrowUp") {
+    menuTwo.classList.remove("active-item");
+    menuCpu.classList.add("active-item");
+  } else if (
+    menuInstruct.classList.contains("active-item") &&
+    e.key === "ArrowUp"
+  ) {
+    menuInstruct.classList.remove("active-item");
+    menuTwo.classList.add("active-item");
+  }
+
+  if (menuTwo.classList.contains("active-item") && e.key === "Enter") {
+    menuView.classList.add("hiddenView");
+    gameView.classList.remove("hiddenView");
+  }
+});

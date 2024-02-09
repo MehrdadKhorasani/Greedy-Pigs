6e560c8d9b375fd4970bc592515953a027d4a3f3
MehrdadKhorasani <mehrdadaria9@gmail.com>
2024-02-09 20:36:23 +0330

    JS file merged

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,15 @@
 "use strict";
 
+//views
+const menuView = document.querySelector(".main-menu");
+const gameView = document.querySelector(".main-game");
+const instructView = document.querySelector(".main-instruction");
+
+//Menu View
+const menuCpu = document.getElementById("menu-item-cpu");
+const menuTwo = document.getElementById("menu-item-two");
+const menuInstruct = document.getElementById("menu-item-ins");
+
 const score0 = document.getElementById("score--0");
 const score1 = document.getElementById("score--1");
 const current0 = document.getElementById("current--0");
@@ -18,6 +28,43 @@ const keyHold = document.querySelector(".key--hold");
 
 let scores, currentScore, activePlayer, playing, double;
 if (gameView.classList.contains("hiddenView")) !playing;
+
+window.addEventListener("load", function () {
+  gameView.classList.add("hiddenView");
+  menuView.classList.remove("hiddenView");
+});
+
+document.addEventListener("keydown", function (e) {
+  if (menuCpu.classList.contains("active-item") && e.key === "ArrowDown") {
+    menuCpu.classList.remove("active-item");
+    menuTwo.classList.add("active-item");
+  } else if (
+    menuTwo.classList.contains("active-item") &&
+    e.key === "ArrowDown"
+  ) {
+    menuTwo.classList.remove("active-item");
+    menuInstruct.classList.add("active-item");
+  } else if (menuTwo.classList.contains("active-item") && e.key === "ArrowUp") {
+    menuTwo.classList.remove("active-item");
+    menuCpu.classList.add("active-item");
+  } else if (
+    menuInstruct.classList.contains("active-item") &&
+    e.key === "ArrowUp"
+  ) {
+    menuInstruct.classList.remove("active-item");
+    menuTwo.classList.add("active-item");
+  }
+
+  if (menuTwo.classList.contains("active-item") && e.key === "Enter") {
+    menuView.classList.add("hiddenView");
+    gameView.classList.remove("hiddenView");
+  }
+
+  if (menuInstruct.classList.contains("active-item") && e.key === "Enter") {
+    menuView.classList.add("hiddenView");
+    instructView.classList.remove("hiddenView");
+  }
+});
 
 function init() {
   scores = [0, 0];

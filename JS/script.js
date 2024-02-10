4c194588cca6730544a3bbd1d06defa0aec3a60b
MehrdadKhorasani <mehrdadaria9@gmail.com>
2024-02-10 21:09:48 +0330

    fix the 100 point bug

--- JS/script.js
+++ JS/script.js
@@ -4,12 +4,55 @@
 const menuView = document.querySelector(".main-menu");
 const gameView = document.querySelector(".main-game");
 const instructView = document.querySelector(".main-instruction");
-
 //Menu View
 const menuCpu = document.getElementById("menu-item-cpu");
 const menuTwo = document.getElementById("menu-item-two");
 const menuInstruct = document.getElementById("menu-item-ins");
 
+window.addEventListener("load", menu);
+
+function menu() {
+  gameView.classList.add("hiddenView");
+  instructView.classList.add("hiddenView");
+  menuView.classList.remove("hiddenView");
+  playing = false;
+  document.addEventListener("keydown", function (e) {
+    if (menuCpu.classList.contains("active-item") && e.key === "ArrowDown") {
+      menuCpu.classList.remove("active-item");
+      menuTwo.classList.add("active-item");
+    } else if (
+      menuTwo.classList.contains("active-item") &&
+      e.key === "ArrowDown"
+    ) {
+      menuTwo.classList.remove("active-item");
+      menuInstruct.classList.add("active-item");
+    } else if (
+      menuTwo.classList.contains("active-item") &&
+      e.key === "ArrowUp"
+    ) {
+      menuTwo.classList.remove("active-item");
+      menuCpu.classList.add("active-item");
+    } else if (
+      menuInstruct.classList.contains("active-item") &&
+      e.key === "ArrowUp"
+    ) {
+      menuInstruct.classList.remove("active-item");
+      menuTwo.classList.add("active-item");
+    }
+    if (menuTwo.classList.contains("active-item") && e.key === "Enter") {
+      menuView.classList.add("hiddenView");
+      gameView.classList.remove("hiddenView");
+      init();
+    }
+
+    if (menuInstruct.classList.contains("active-item") && e.key === "Enter") {
+      menuView.classList.add("hiddenView");
+      instructView.classList.remove("hiddenView");
+    }
+  });
+}
+
+//Game
 const score0 = document.getElementById("score--0");
 const score1 = document.getElementById("score--1");
 const current0 = document.getElementById("current--0");
@@ -27,44 +70,6 @@ const keyRoll = document.querySelector(".key--roll");
 const keyHold = document.querySelector(".key--hold");
 
 let scores, currentScore, activePlayer, playing, double;
-if (gameView.classList.contains("hiddenView")) !playing;
-
-window.addEventListener("load", function () {
-  gameView.classList.add("hiddenView");
-  menuView.classList.remove("hiddenView");
-});
-
-document.addEventListener("keydown", function (e) {
-  if (menuCpu.classList.contains("active-item") && e.key === "ArrowDown") {
-    menuCpu.classList.remove("active-item");
-    menuTwo.classList.add("active-item");
-  } else if (
-    menuTwo.classList.contains("active-item") &&
-    e.key === "ArrowDown"
-  ) {
-    menuTwo.classList.remove("active-item");
-    menuInstruct.classList.add("active-item");
-  } else if (menuTwo.classList.contains("active-item") && e.key === "ArrowUp") {
-    menuTwo.classList.remove("active-item");
-    menuCpu.classList.add("active-item");
-  } else if (
-    menuInstruct.classList.contains("active-item") &&
-    e.key === "ArrowUp"
-  ) {
-    menuInstruct.classList.remove("active-item");
-    menuTwo.classList.add("active-item");
-  }
-
-  if (menuTwo.classList.contains("active-item") && e.key === "Enter") {
-    menuView.classList.add("hiddenView");
-    gameView.classList.remove("hiddenView");
-  }
-
-  if (menuInstruct.classList.contains("active-item") && e.key === "Enter") {
-    menuView.classList.add("hiddenView");
-    instructView.classList.remove("hiddenView");
-  }
-});
 
 function init() {
   scores = [0, 0];
@@ -104,6 +109,7 @@ document.addEventListener("keydown", function (e) {
       menuView.classList.remove("hiddenView");
       gameView.classList.add("hiddenView");
       instructView.classList.add("hiddenView");
+      break;
   }
 });
 
@@ -123,8 +129,8 @@ function switchPlayer() {
 
 function ruleChecker(firstDice, secondDice, score) {
   if (firstDice + secondDice === 7) return "loseCurrent";
-  if (firstDice === 1 && secondDice === 1) return "loseTotal";
   if (firstDice + secondDice + score === 100) return "loseTotal";
+  if (firstDice === 1 && secondDice === 1) return "loseTotal";
   if (firstDice === secondDice) return "hasDouble";
   return "OK";
 }
@@ -147,7 +153,6 @@ function roll() {
         break;
       case "loseTotal":
         scores[activePlayer] = 0;
-        // currentScore = 0;
         document.getElementById(`score--${activePlayer}`).textContent = 0;
         document.getElementById(`current--${activePlayer}`).textContent = 0;
         switchPlayer();
@@ -165,6 +170,12 @@ function roll() {
         currentScore += firstDice + secondDice;
         document.getElementById(`current--${activePlayer}`).textContent =
           currentScore;
+        if (currentScore + scores[activePlayer] === 100) {
+          scores[activePlayer] = 0;
+          document.getElementById(`score--${activePlayer}`).textContent = 0;
+          document.getElementById(`current--${activePlayer}`).textContent = 0;
+          switchPlayer();
+        }
     }
   }
 }

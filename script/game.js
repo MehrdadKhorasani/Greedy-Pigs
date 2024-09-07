87fd863bc33d0e2071be6423f13c446c620eb5d9
Mehrdad Khorasani <mehrdadaria9@gmail.com>
2024-09-07 14:43:16 +0330

    add all logic and game ui init

--- script/game.js
+++ script/game.js
@@ -1,4 +1,6 @@
-import { initGameUI } from './game_UI.js'
+import { game_view, initGameUI } from './game_UI.js'
+import { initMenu } from "./menu_UI.js";
+import { menu_item_cpu as single_player } from './menu_UI.js';
 
 const state = {
   scores: [0, 0],
@@ -8,7 +10,7 @@ const state = {
 
 let playing = true;
 let active_player = 0;
-let single_mode = false;
+export let single_mode = false;
 let is_double = false;
 
 // Initialize the game logic
@@ -22,8 +24,151 @@ function initGame() {
 }
 
 export function gameFunction() {
-  initGame()
+  // if (playing) 
+  single_mode = single_player.classList.contains('active-item') ? true : false;
+  initGame();
+  document.addEventListener('keydown', keyHandler);
 }
 
-// FN: Change the player
-const changeTurn = () => active_player = active_player === 0 ? 1 : 0;
+// fn: Change the player
+const switchPlayer = () => active_player = active_player === 0 ? 1 : 0;
+
+// fn: last roll added to last_rolls array (checking doubles)
+function addLastRoll() {
+  state.last_rolls.push(is_double);
+  state.last_rolls.shift();
+}
+
+// fn: add the roll score to current score
+function addCurrentScore(num1, num2) {
+  state.current_score += Number(num1 + num2);
+}
+
+// fn: add the roll score to current score
+function addScore() {
+  state.scores[active_player] += state.current_score;
+}
+
+// fn: Random dice
+function randomDice() {
+  const firstNumber = Math.ceil(Math.random() * 6);
+  const secondNumber = Math.ceil(Math.random() * 6);
+  return { firstNumber, secondNumber };
+}
+
+// fn: check the rulls of the game:
+function rullChecker(num1, num2) {
+  if (num1 === num2 && num1 !== 1) return 'double';
+  if (num1 + num2 === 7) return 'lose_turn';
+  if (num1 === 1 && num2 === 1) return 'lose_scores';
+  return 'ok'
+}
+
+// player loses the turn
+function loseTurn() {
+  state.current_score = 0;
+  changeTurn()
+}
+
+// player loses the turn and all the score
+function loseAll() {
+  state.scores[active_player] = 0;
+  loseTurn()
+}
+
+// fn: if the players get doubles
+function double() {
+  is_double = true;
+  const double_count = state.last_rolls.filter(value => value === true).length;
+  if (double_count === 2) {
+    state.last_rolls.splice(0, state.last_rolls.length, false, false)
+    // lose all
+    switchPlayer()
+    return;
+  }
+  addCurrentScore(num1, num2);
+  addLastRoll(is_double)
+
+  // delete this:
+  roll()
+}
+
+// fn: handling the key events
+function keyHandler(keydown) {
+  if (game_view.classList.contains('hiddenView')) return;
+  const key = keydown.key;
+  switch (key) {
+    case 'r':
+      roll();
+      break;
+    case 'h':
+      hold();
+      break;
+    case 'n':
+      // new Game
+      console.log('new game');
+      initMenu();
+      // stop all the current functionality.
+      break;
+    case 'Escape':
+      initGame();
+      initMenu();
+      document.removeEventListener('keydown', keyHandler);
+      break;
+    default:
+      console.log("please enter a valid input");
+  }
+}
+
+// fn: roll
+function roll() {
+  const { firstNumber: num1, secondNumber: num2 } = randomDice();
+
+  const result = rullChecker(num1, num2);
+
+  if (result === 'double') {
+    double();
+    // MUST Continue
+  } else {
+    is_double = false;
+    addLastRoll()
+
+    if (result === 'lose_turn') loseTurn();
+    if (result === 'lose_scores') loseAll();
+    if (result === 'ok') addCurrentScore(num1, num2);
+  }
+}
+
+// fn: hold
+function hold() {
+  if (!is_double) {
+    addScore()
+    state.current_score = 0;
+    switchPlayer()
+  }
+}
+
+// check if there's a winner
+function checkWinner() {
+  if (state.scores[active_player] > 100) {
+    endGame()
+  }
+
+  //move this functionality to roll function:
+  if (state.scores[active_player] + state.current_score === 100) {
+    // Remember: after every roll, the current score updated (if the status == ok), after that the condition must be checked.
+    // if the condition checked before that, the current score is not correct
+    // LOSE ALL
+  }
+}
+
+// end of the game
+function endGame() {
+  playing = false;
+  console.log("we've got a Winner.")
+}
+
+
+// update UI
+function updateUI({ data }) {
+}
--- script/game_UI.js
+++ script/game_UI.js
@@ -1,4 +1,5 @@
 import { menu_view } from './menu_UI.js'
+import { single_mode } from './game.js';
 // View:
 export const game_view = document.querySelector('.main-game');
 
@@ -25,4 +26,33 @@ const keyHold = document.querySelector(".key--hold");
 export function initGameUI() {
   menu_view.classList.add("hiddenView");
   game_view.classList.remove("hiddenView");
+
+  score0.textContent = score1.textContent = current0.textContent = current1.textContent = 0;
+  statusMessage();
+  playerName.textContent = 'Player-1';
+  cpuName.textContent = single_mode ? 'npc' : 'Player-2';
+}
+
+function statusMessage(message = "Roll the Dices to start !!") {
+  let text;
+  switch (message) {
+    case 'roll':
+      text = 'Roll the dice';
+      break;
+    case 'hold':
+      text = 'You holded the score'
+    case 'load':
+      text = 'Please wait...'
+      break;
+    case 'double':
+      text = 'Double Double !!'
+      break;
+    case 'no-hold':
+      text = 'After doubles, you must roll!'
+    case 'win':
+      text = 'ss' // check the winner and named it with regex
+    default:
+      text = message;
+  }
+  gameStatus.textContent = text;
 }
